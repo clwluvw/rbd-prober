f669b3cf32a73358e806e3b50597adfd6fe61731
Seena Fallah <seenafallah@gmail.com>
2020-12-26 03:56:12 +0330

    feat: add rbd prober
    Signed-off-by: Seena Fallah <seenafallah@gmail.com>

---
+++ main.py
@@ -0,0 +1,27 @@
+import yaml
+import argparse
+import time
+import logging
+
+from rbd_prober.prober import RBDProber
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, help="config file path", default="config.yaml")
+    args = parser.parse_args()
+
+    with open(args.config) as config_file:
+        configs = yaml.full_load(config_file)
+        rbd_prober = RBDProber(**configs)
+        interval = int(configs.get('interval'))
+    
+    while True:
+        logging.debug(f"sleep {interval}")
+        time.sleep(interval)
+        rbd_prober.probe()
+    
+
+if __name__ == "__main__":
+    main()
---
+++ rbd_prober/prober.py
@@ -0,0 +1,108 @@
+import logging
+from datetime import datetime
+
+import rados
+import rbd
+
+from .exceptions import InternalError
+
+
+class Prober(object):
+    def __init__(self, *args, **kwargs):
+        self.type = kwargs.get('type')
+        self._validate_type()
+        self.object_size = int(kwargs.get('object_size'))
+        
+    def _validate_type(self):
+        if self.type != 'write' and self.type != 'read':
+            raise TypeError("prober type should be one of the write or read")
+
+
+class RBDProber(object):
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.get('name')
+        self.interval = kwargs.get('interval')
+        self.prober = Prober(**kwargs.get('prober'))
+        self.pool_name = kwargs.get('pool_name')
+        self.image_name = kwargs.get('image_name')
+        self.rbd_user = kwargs.get('rbd_user')
+        self.rbd_keyring_path = kwargs.get('rbd_keyring_path')
+        self.monitors = kwargs.get('monitors')
+        
+        rados_connection = self._connect_to_rados()
+        self.image_ioctx = self._open_image(rados_connection)
+
+    def _connect_to_rados(self):
+        cluster = rados.Rados(rados_id=self.rbd_user, conf={
+            "mon_host": ",".join(self.monitors),
+            "keyring": self.rbd_keyring_path,
+            "rbd_cache": "false",
+        })
+        cluster.connect()
+        return cluster
+    
+    def _open_image(self, rados_connection):
+        ioctx = rados_connection.open_ioctx(self.pool_name)
+        return rbd.Image(ioctx, self.image_name)
+
+    def probe(self):
+        logging.debug("start probing")
+        response_time = -1
+
+        try:
+            if self.prober.type == "write":
+                response_time = self.write()
+            elif self.prober.type == "read":
+                response_time = self.read()
+        except InternalError:
+            return
+        
+        logging.info(f"probbing finished response_time: {response_time}")
+
+    def write(self):
+        logging.debug("start write probe")
+
+        try:
+            self.image_ioctx.discard(0, self.prober.object_size)
+        except Exception:
+            logging.exception("failed to discard previous write")
+            raise InternalError()
+
+        data = b'1' * self.prober.object_size
+        logging.debug("data created for write")
+        start_time = datetime.now()
+        try:
+            n = self.image_ioctx.write(data, 0)
+            end_time = datetime.now()
+        except Exception:
+            logging.exception("failed to write to rbd image")
+            return -1
+        logging.debug(f"write finished data_size: {n}")
+
+        response_time = (end_time - start_time).total_seconds()
+        logging.debug(f"response time calculated response_time: {response_time}")
+        return response_time
+
+    def read(self):
+        logging.debug("start read probe")
+
+        data = b'1' * self.prober.object_size
+        try:
+            self.image_ioctx.write(data, 0)
+        except Exception:
+            logging.exception("failed to prepare data to for read")
+            raise InternalError()
+        logging.debug("data has been written to be read")
+
+        start_time = datetime.now()
+        try:
+            self.image_ioctx.read(0, self.prober.object_size)
+            end_time = datetime.now()
+        except Exception:
+            logging.exception("failed to read data from rbd image")
+            return -1
+        logging.debug("read finished")
+
+        response_time = (end_time - start_time).total_seconds()
+        logging.debug(f"response time calculated response_time: {response_time}")
+        return response_time
