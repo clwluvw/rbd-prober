6050a531a24b7df004c0b9890b2e6ba374b7a411
Seena Fallah <seenafallah@gmail.com>
2021-10-05 14:19:55 +0330

    refactor: use python simple server instead of flask and gunicorn
    Signed-off-by: Seena Fallah <seenafallah@gmail.com>

---
+++ app.py
@@ -0,0 +1,62 @@
+from wsgiref.simple_server import make_server
+import yaml
+
+from prometheus_client import make_wsgi_app
+
+from rbd_prober.prober import RBDProber, PrometheusExporter
+
+
+def home():
+    status = '200 OK'
+    header = ('Content-Type', 'text/html')
+    output = b'''<html>
+        <head><title>RBD Prober</title></head>
+        <body>
+        <h1>RBD Prober</h1>
+        <p><a href="/metrics">Metrics</a></p>
+        </body>
+        </html>'''
+    return status, header, output
+
+
+def not_found():
+    status = '404 Not Found'
+    header = ('Content-Type', 'application/openmetrics-text')
+    output = b''
+    return status, header, output
+
+
+def wsgi():
+    def exporter_app(environ, start_response):
+        if environ['PATH_INFO'] == '/metrics':
+            app = make_wsgi_app()
+            return app(environ, start_response)
+
+        if environ['PATH_INFO'] == '/':
+            status, header, output = home()
+        else:
+            status, header, output = not_found()
+
+        start_response(status, [header])
+        return [output]
+    return exporter_app
+
+
+with open("./config.yaml") as config_file:
+    configs = yaml.full_load(config_file)
+
+    # exporer config
+    exporter_host = configs['exporter_host']
+    exporter_port = configs['exporter_port']
+
+    # init exporter metrics
+    PrometheusExporter.getInstance().init_metrics(configs['histogram_buckets'])
+
+    for config in configs['probs']:
+        rbd_prober = RBDProber(**config)
+        rbd_prober.start()
+
+
+app = wsgi()
+httpd = make_server(exporter_host, exporter_port, app)
+httpd.serve_forever()
--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = '0.0.0.0:8000'
-workers = 1
-errorlog = '-'
-accesslog = '-'
-proc_name = 'rbd_prober'
